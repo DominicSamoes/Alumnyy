be7ef982f00b983e8361f6bfeafcfdbc1deb559d
DominicSamoes <dominicsamo@gmail.com>
2022-11-30 00:14:16 +0200

    App usability improvement

--- webapp/views.py
+++ webapp/views.py
@@ -1,16 +1,22 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import user as User
+from  sqlalchemy.sql.expression import func, select
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template("index.html")
+    """Display six random alumni"""
+    alumni = User.query.order_by(func.rand()).limit(8)
+    return render_template("index.html", alumni=alumni)
 
 @views.route('/home')
 @login_required 
 def home():
-    return render_template("home.html", user=current_user)
+    """Display six random alumni"""
+    alumni = User.query.filter_by().order_by(func.rand()).limit(8)
+    return render_template("home.html", user=current_user, alumni=alumni)
 
 @views.route('/about')
 def about():
